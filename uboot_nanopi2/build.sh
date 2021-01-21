8128d2e5a380a3b05a194ac2b2013149fd7aa668
wyk <flying_dolphin@163.com>
2021-01-21 09:04:39 -0800

    use gcc 4.9.3 version toolchain

--- uboot_nanopi2/build.sh
+++ uboot_nanopi2/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 make clean
 make s5p6818_nanopi3_config
-make CROSS_COMPILE=/opt/toolchain/arm-cortex_a9-eabi-4.7-eglibc-2.18/bin/arm-cortex_a9-linux-gnueabi- -j4
+make CROSS_COMPILE=/opt/toolchain/arm-cortex_a8-linux-gnueabi-4.9.3/bin/arm-cortex_a8-linux-gnueabi- -j4
