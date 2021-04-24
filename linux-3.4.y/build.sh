968e8301cbb03c017c34848724ec9124547ea572
wyk <flying_dolphin@163.com>
2021-04-24 07:29:23 -0700

    Add linux-3.4.y/build.sh

---
+++ linux-3.4.y/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#cp arch/arm/configs/s5p6818_nanopi3_linux_wyk_config .config
+make ARCH=arm CROSS_COMPILE=/opt/toolchain/arm-cortex_a8-linux-gnueabi-4.9.3/bin/arm-cortex_a8-linux-gnueabi- uImage -j4
